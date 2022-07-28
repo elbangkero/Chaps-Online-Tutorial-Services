67d404d1561f7d9e9e07b3921f853ec563c6c8a2
Robert Gajelomo <robert.gajelomo@everlounge.net>
2022-07-28 18:34:28 +0800

    fix header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,16 +11,15 @@
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('public/storage/img/image0.jpeg')}}" />
 
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Google Web Fonts --> 
+    <link href="{{asset('public/storage/css/fonts.googleapis.css')}}" rel="stylesheet">
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
     <link href="{{asset('storage/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-
+ 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{asset('public/storage/css/style.css')}}" rel="stylesheet">
 </head>
@@ -52,11 +51,24 @@
 
 
     <!-- Navbar Start -->
-    <div class="container-fluid p-5">
+    <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
             <a href="index.html" class="navbar-brand ml-lg-3">
-                <h1 class="m-0 display-5 text-uppercase text-primary"><u> Chaps Online Tutorial Services </u></h1>
+                <h5 class="m-0 display-10 text-uppercase text-primary"><u>Chaps Online Tutorial Services </u> </h5>
             </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
+                <div class="navbar-nav m-auto py-0">
+                    <a href="index.html" class="nav-item nav-link active">Home</a>
+                    <a href="about.html" class="nav-item nav-link">About</a>
+                    <a href="service.html" class="nav-item nav-link">Service</a>
+                    <a href="price.html" class="nav-item nav-link">Price</a> 
+                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                </div>
+                <a href="" class="btn btn-primary py-2 px-4 d-none d-lg-block">Register Here!</a>
+            </div>
         </nav>
     </div>
     <!-- Navbar End -->
