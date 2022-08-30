711a4fecc2ac60f9eda1d96c1ab12444d2e8747c
Robert Gajelomo <robert.gajelomo@everlounge.net>
2022-08-30 14:25:00 +0800

    one device only per login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RedirectIfAuthenticated
 {
@@ -26,7 +27,10 @@ class RedirectIfAuthenticated
                 return redirect(RouteServiceProvider::HOME);
             }
         }
-
+        $user_id =   DB::table('users')->select('id')->where('email', '=', $request->input('email'))->first();
+        DB::table('sessions')->where('user_id', $user_id->id)->delete();
         return $next($request);
+        //dd($request->input('email'));
+
     }
 }
